209afb0a1aa6e5d7ebd50034085b6f3f4b61baf4
Mika Tuupola <tuupola@appelsiini.net>
2021-11-12 16:18:16 +0200

    Sync rotozoom from ESP Effects

--- rotozoom.c
+++ rotozoom.c
@@ -2,7 +2,7 @@
 
 MIT No Attribution
 
-Copyright (c) 2020 Mika Tuupola
+Copyright (c) 2020-2021 Mika Tuupola
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,14 +26,13 @@ SPDX-License-Identifier: MIT-0
 */
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
 #include <hagl.h>
 
 #include "head.h"
 
 static const uint8_t SPEED = 2;
-static const uint8_t STEP = 1;
+static const uint8_t PIXEL_SIZE = 2;
 
 static uint16_t angle = 0;
 // static float sinlut[360];
@@ -58,8 +57,8 @@ void rotozoom_render()
     // c = coslut[angle];
     z = s * 1.2;
 
-    for (uint16_t x = 0; x < DISPLAY_WIDTH; x = x + STEP) {
-        for (uint16_t y = 0; y < DISPLAY_HEIGHT; y = y + STEP) {
+    for (uint16_t x = 0; x < DISPLAY_WIDTH; x = x + PIXEL_SIZE) {
+        for (uint16_t y = 0; y < DISPLAY_HEIGHT; y = y + PIXEL_SIZE) {
 
             /* Get a rotated pixel from the head image. */
             int16_t u = (int16_t)((x * c - y * s) * z) % HEAD_WIDTH;
@@ -71,7 +70,11 @@ void rotozoom_render()
             }
             color_t *color = (color_t*) (head + HEAD_WIDTH * sizeof(color_t) * v + sizeof(color_t) * u);
 
-            hagl_put_pixel(x, y, *color);
+            if (1 == PIXEL_SIZE) {
+                hagl_put_pixel(x, y, *color);
+            } else {
+                hagl_fill_rectangle(x, y, x + PIXEL_SIZE - 1, y + PIXEL_SIZE - 1, *color);
+            }
         }
     }
 }
